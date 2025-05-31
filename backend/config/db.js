be17d6891ee9611e200aa0a43d0e95aac7fa841f
Moin Hasanfatta <hasanfattamoin@gmail.com>
2025-05-31 22:12:39 +0530

    backend fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-export const connectDB = async ()=>{
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`DB connected : ${conn.connection.host}`)
-    } catch{
-        console.error(`Error : ${error.message}`)
-        process.exit(1) // 1 failure and 0 success
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect('mongodb://localhost:27017/ecommerce', {
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit process with failure
     }
-}
+};
+
+export default connectDB;
